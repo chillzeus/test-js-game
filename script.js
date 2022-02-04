e3116e6dffd1036a178775d9cdb94a921b023145
ChillZeus <76137066+ChillZeus@users.noreply.github.com>
2022-02-04 11:26:43 -0600

    This is an experimental commit probably won't work

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let canvas = document.getElementById("myCanvas")
 let ctx = canvas.getContext("2d")
 let x = 40;
-//second level is 225;
-let y = 225;
+let y = 293;
 let dx = 2;
 let dy = 2;
 let leftPressed = false;
@@ -11,7 +10,8 @@ let upPressed = false;
 let downPressed = false;
 let collision = false;
 let timeVariable = 5;
-let floor = 292;
+let jumped = false;
+let floor = 285;
 
 //remember to draw the stars
 
@@ -20,25 +20,12 @@ function drawStars () {
 }
 
 function drawMap() {
-
 	//draws floor
-  //ctx.beginPath();ctx.rect(0, 308, 500, 30);ctx.fillStyle = "#9c6319";ctx.fill();ctx.closePath();
-  drawPath(0, 308, 500, 30, "#9c6319");
+  ctx.beginPath();ctx.rect(0, 308, 500, 30);ctx.fillStyle = "#9c6319";ctx.fill();ctx.closePath();
   //this draws the walkways
-  //ctx.beginPath();ctx.rect(0, 240, 500, 10);ctx.fillStyle = "Blue";ctx.fill();ctx.closePath();
-  drawPath(0, 240, 500, 10, "Blue");
-  drawPath(0, 170, 500, 10, "Blue");
-  drawPath(0, 100, 500, 10, "Blue");
-  //draws the ladders
-  drawPath(430, 252, 15, 55,"Grey");
-}
-
-function drawPath(x, y, w, h, fillStyle) {
-  ctx.beginPath();
-  ctx.rect(x, y, w, h);
-  ctx.fillStyle = fillStyle;
-  ctx.fill();
-  ctx.closePath();
+  ctx.beginPath();ctx.rect(0, 240, 500, 10);ctx.fillStyle = "Blue";ctx.fill();ctx.closePath();
+  ctx.beginPath();ctx.rect(0, 170, 500, 10);ctx.fillStyle = "Blue";ctx.fill();ctx.closePath();
+  ctx.beginPath();ctx.rect(0, 100, 500, 10);ctx.fillStyle = "Blue";ctx.fill();ctx.closePath();
 }
 
 function drawMan() {
@@ -59,8 +46,6 @@ function keyDownHandler(e) {
   	rightPressed = true;
   } if (e.key == "ArrowUp") {
       upPressed = true;
-  } if (e.key == "ArrowDown") {
-      downPressed = true;
   }
 }
 function keyUpHandler(e) {
@@ -70,8 +55,6 @@ function keyUpHandler(e) {
   	rightPressed = false;
   } if (e.key == "ArrowUp") {
     upPressed = false;
-  } if (e.key == "ArrowDown") {
-    downPressed = false;
   }
 }
 
@@ -81,7 +64,6 @@ function draw() {
   drawMan();
   timeVariable += 1;
 
-  //this is where collision detection will be
   if (x < 0) {
     collision = true;
     dx = 0;
@@ -98,7 +80,8 @@ function draw() {
         dx = 2;
       }
     }
-  if (y > 292) {
+// in theory the y works
+  if (y > 285) {
     collision = true;
     dy = 0;
     if (upPressed == true) {
@@ -111,18 +94,22 @@ function draw() {
   } if (rightPressed == true) {
   	x += dx;
   } if (upPressed == true) {
+    y += dy;
+    jumped = true;
+  } if (downPressed == true) {
     y += -dy;
-    if (collision == true && downPressed == false) {
-      
+  }
+
+  if (jumped == true) {
+    dy = 0;
+    if (y > floor + 20) {
+      for (let i = 0; i > 20; i++) {
+        y += 1;
+      }
+      jumped = false;
+      dy = 2;
     }
   }
-  if (downPressed == true) {
-    y += dy;   
-  }
-  //remember new variable for levels (floor??)
-  if (y < floor) {
-    y += 0.7;
-  }
-}  
+}
 
 let interval = setInterval(draw, 10);
